45900cfd83af82d409237a65ba4515c6ff282d37
fikohayuanuroso <81566618+fikohayuanuroso@users.noreply.github.com>
2022-08-10 13:04:45 +0700

    Initial commit

---
+++ app/src/main/java/com/paymu/fipay/MainActivity.java
@@ -0,0 +1,37 @@
+package com.paymu.fipay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    String username = "fikohayuanuroso";
+    String password = "0502";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText txtUser = findViewById(R.id.txtUser);
+        final EditText txtPass = findViewById(R.id.txtPass);
+        Button login = findViewById(R.id.Login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+             if (txtUser.getText().toString().equalsIgnoreCase(username)&& txtPass.getText().toString().equalsIgnoreCase(password)) {
+                 startActivity(new Intent(MainActivity.this, home.class));
+             }else{
+                 Toast.makeText(MainActivity.this, "username/password", Toast.LENGTH_SHORT).show();
+             }
+         }
+        });
+    }
+}
